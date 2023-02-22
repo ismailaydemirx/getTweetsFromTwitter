acb7a9a3a5d65b357219f7e47ce8a1d1f239a19e
Ismail Aydemir <theangelo112@gmail.com>
2023-02-22 18:45:57 +0300

    Datas to excel file features added.

--- tweets.py
+++ tweets.py
@@ -1,15 +1,21 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import openpyxl
+import pytz
+from datetime import datetime
 
-query= "(from:elonmusk)"
-tweets=[]
-limits=30
+query = "(from:elonmusk)"
+tweets = []
+limits = 30
+
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-
-    if len(tweets)==limits:
+    if len(tweets) == limits:
         break
-    tweets.append([tweet.date,tweet.username, tweet.content])
+    tweet_date = tweet.date
+    if tweet_date.tzinfo is not None:
+        tweet_date = tweet_date.astimezone(pytz.utc).replace(tzinfo=None)
+    tweets.append([tweet_date, tweet.username, tweet.content])
 
-df = pd.DataFrame(tweets,columns=['Date','User','Tweet'])
-print(df)
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+df.to_excel('tweets.xlsx', index=False, sheet_name="TweetsFromElonMusk")
+print("Excel file created successfully.")
