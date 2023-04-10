4fa9aa102072b56a740d9f3b9fb98e66573d652c
Ismail Aydemir <theangelo112@gmail.com>
2023-04-10 20:50:48 +0300

    Update tweets.py
    This code is a Python program that retrieves a specified number of tweets from a given Twitter username and writes them into an Excel file. It uses the snscrape and pandas libraries to add the tweets into a DataFrame and write them into an Excel file. It also removes the timezone information from the dates of the tweets. The code has a robust structure that can handle various errors and provides debugging messages. Additionally, it has additional features such as retrieving data from multiple accounts and setting the name of the Excel file based on the account names automatically.

--- tweets.py
+++ tweets.py
@@ -1,21 +1,28 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
-import openpyxl
-import pytz
-from datetime import datetime
 
-query = 'from:elonmusk OR from:twitter'
-tweets = []
-limits = 10
+# Kullanıcı adları
+usernames = ['twitter', 'elonmusk']
 
-for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-    if len(tweets) == limits:
-        break
-    tweet_date = tweet.date
-    if tweet_date.tzinfo is not None:
-        tweet_date = tweet_date.astimezone(pytz.utc).replace(tzinfo=None)
-    tweets.append([tweet_date, tweet.username, tweet.content + " YourBrand"])
+# Kaç tweet çekileceği
+tweet_count = 50
 
-df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
-df.to_excel('tweets.xlsx', index=False, sheet_name="TweetsFromBPTHaberAndPusholder")
-print(df)
+# Tweetleri bir DataFrame'e ekleme
+tweets_list = []
+for username in usernames:
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+username).get_items()):
+        if i >= tweet_count:
+            break
+        tweets_list.append([tweet.date, tweet.username, tweet.content])
+df = pd.DataFrame(tweets_list, columns=['date', 'username', 'tweet'])
+
+# Timezone bilgilerini atma
+df['date'] = df['date'].apply(lambda x: x.replace(tzinfo=None))
+
+# Excel dosyasına yazdırma
+excel_filename = '_'.join(usernames) + '_tweets.xlsx'
+with pd.ExcelWriter(excel_filename) as writer:
+    df.to_excel(writer, index=False)
+
+# Başarılı bir şekilde kaç tweet çekildiğini ve hangi hesaplardan çekildiğini yazdırma
+print(f"{tweet_count} tweet {', '.join(usernames)} hesaplarından başarılı bir şekilde çekildi.")
